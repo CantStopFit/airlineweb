8dea60ff3e28678b8f0a3f0056b99eb3c3dbe7eb
Greyson Hubler <greysonhubler@gmail.com>
2023-12-29 15:00:58 -0600

    second update

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Airline Website</title>
-    <link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-</head>
-<body>
+<?php
+    include_once 'header.php'
+?>
     <main class="hero">
         <img src="imgs/americanPlaneAtGateCroped.JPG" alt="" id="desktop-hero-image">
         <img src="imgs/IMG_1331.WEBP" alt="" id="mobile-hero-image">
@@ -32,5 +25,6 @@
         </section>
         <div class="spacer"></div>
     </section>
-</body>
-</html>
+<?php 
+    include_once 'footer.php'
+?>
---
+++ navagations/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Airline Website</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/navagations.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <div class="header">
+    <div class="header-branding">
+        <img src="imgs/aaLogo.png" alt="">   
+        <h1>American Airlines</h1>
+    </div>
+    <div class="header-navagation">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="index.php">Flights</a></li>
+            <li><a href="index.php">Hotels</a></li>
+            <li><a href="index.php">Cars</a></li>
+            <li><a href="index.php">Signup</a></li>
+            <li><a href="index.php">Login</a></li>
+        </ul>
+    </div> 
+    </div>
