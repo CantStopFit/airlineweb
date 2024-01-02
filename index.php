f35e0e6b5c36435d80277d147cf0a90804f5b8cf
Greyson Hubler <greysonhubler@gmail.com>
2024-01-02 17:41:33 -0600

    push six/image-updates

--- index.php
+++ index.php
@@ -13,7 +13,11 @@
         }
         ?>
 
-        <img src="imgs/untitled-design.png" alt="" width="100%">
+        <img src="imgs/hero/landing-in-mountains.png" alt="">
+        <img src="imgs/hero/plane-take-off-twr-back.png" alt="">
+        <img src="imgs/hero/single-plane-parked-at-gate.png" alt="">
+        <img src="imgs/hero/single-plane-parked-back.png" alt="">
+        <img src="imgs/hero/two-planes-at-gate.png" alt="">
 <?php 
     include_once 'navagations/footer.php'
 ?>
